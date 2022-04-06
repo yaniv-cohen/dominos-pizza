28386b440629bf309eca7f84df9f4240e1a8b1a0
yanivfast1 <45695126+yanivfast1@users.noreply.github.com>
2022-04-06 17:29:15 +0300

    Update myscript.js

--- dough_select/myscript.js
+++ dough_select/myscript.js
@@ -1,4 +1,4 @@
 function next(){
     // ('C:\Users\כללי\Desktop\coding\dominos_order_page\topping_select');
-    window.location.href =("C:\\Users\\כללי\\Desktop\\coding\\dominos_order_page\\topping_select\\index.html");
+    window.location.href =("\\dominos_order_page\\topping_select\\index.html");
 }
